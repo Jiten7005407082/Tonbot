080991a7187f4d11f922fc0b32c5c2d02c23567a
THONGAM JITEN SINGH <thongamjitensngh@gmail.com>
2025-07-20 14:55:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 async function fetchViaSheetsAPI() {
   const API_KEY = "YOUR_API_KEY";
   const SHEET_ID = "1YOUR_SHEET_ID";
-  const url = `https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/A:B?key=${API_KEY}`;
+  const url = `https://docs.google.com/spreadsheets/d/e/2PACX-1vSuFI_1eO2D1GqOeZNYAOt1CkzgU4gIXvC_06fMgnhC1BilFUZ_2mgNYD4G_e2XuFD5ylZzYUjgoqTu/pubhtml`;
 
   const response = await fetch(url);
   const data = await response.json();
